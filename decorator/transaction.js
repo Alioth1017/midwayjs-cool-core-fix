20c4530617be0b7a861f19db7e5829ff9cb17bbd
Alioth <z610886764@163.com>
2021-04-29 10:21:57 +0800

    同步代码 for 3.2.5

--- decorator/transaction.js
+++ decorator/transaction.js
@@ -2,15 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CoolTransaction = void 0;
 const typeorm_1 = require("typeorm");
+const comm_1 = require("../exceptions/comm");
 function CoolTransaction(option) {
     return (target, propertyKey, descriptor) => {
+        let queryRunner;
         const method = descriptor.value;
         descriptor.value = async function (...args) {
             let data;
             const connection = typeorm_1.getConnection((option === null || option === void 0 ? void 0 : option.connectionName) || 'default');
-            const queryRunner = connection.createQueryRunner();
+            queryRunner = queryRunner || connection.createQueryRunner();
             // 使用我们的新queryRunner建立真正的数据库连
-            await queryRunner.connect();
+            //await queryRunner.connect();
             if (option && option.isolation) {
                 await queryRunner.startTransaction(option.isolation);
             }
@@ -23,6 +25,7 @@ function CoolTransaction(option) {
             }
             catch (error) {
                 await queryRunner.rollbackTransaction();
+                throw new comm_1.CoolCommException(error.message);
             }
             return data;
         };
